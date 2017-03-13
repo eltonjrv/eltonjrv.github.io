03ccf8ddf8543bc2ee10452bb6b3b86c75603f93
Elton Vasconcelos <eltonjrv@gmail.com>
2017-03-13 10:48:24 -0700

    Add files via upload

--- parseThermoPhyl.pl
+++ parseThermoPhyl.pl
@@ -1,12 +1,22 @@
 #!/usr/bin/perl
 # Programmer: Elton Vasconcelos (10/Mar/2017)
-# Usage: perl parseThermoPhyl.pl [target_species_names.txt] [raw_search_results.txt] [sorted_search_results.txt]
+# Usage: perl parseThermoPhyl.pl [target_species_names.txt] [raw_search_results.txt] [sorted_search_results.txt] [nonTargets_cutoff]
 ################################################################################################################
 # The target_species_names.txt file must be a list of species names (Genus species) one per line.
 # The other two inputs are the default outputs from ThermoPhyl.
-# The output will be named best_candidates.tab, which will contain the primer_pairs that amplify all the target species and less than 1000 non-target sequences.
+# The last argument is an integer corresponding to the maximum number of non-target sequences the user wants to see.
+# The output will be named best_candidates.tsv, which will contain the primer_pairs that amplify all the target species and less than [nonTargets_cutoff] non-target sequences.
 
 use List::MoreUtils qw(uniq);
+
+if (@ARGV != 4) {
+	die ("*** You must specify 4 arguments ***\nUsage: perl parseThermoPhyl.pl [target_species_names.txt] [raw_search_results.txt] [sorted_search_results.txt] [nonTargets_cutoff]\nPlease, read script's header for more instructions.\n");
+}
+
+my $old_file = "best_candidates.tsv";
+if (-f $old_file) {	#If there is already a best_candidates.tsv file within the current directory, it will be removed!
+	`rm best_candidates.tsv`;
+}
 
 open (FILE1, "$ARGV[0]") or die ("Can't open file $ARGV[0]!\n");
 my @species = <FILE1>;
@@ -27,10 +37,12 @@ while ($line ne "") {
 		until ($array[1] != $primerID || $line eq "") {	# Walking on a primerID at a time
 			if ($array[0] eq "N") {		# Counting the number of non-targets
 				$c++;
-			}					
-   			for ($i = 0; $i < @species; $i++) {	
-				if ($line =~ m/$species[$i]/) {	# Finding a match for the target species' name within the raw_search_results.txt
-					push(@caught_species, $species[$i]);
+			}
+			elsif ($array[0] eq "Y") {					
+	   			for ($i = 0; $i < @species; $i++) {	
+					if ($line =~ m/$species[$i]/) {	# Finding a match for the target species' name within the raw_search_results.txt
+						push(@caught_species, $species[$i]);
+					}
 				}
 			}
 			$line = <FILE2>;
@@ -38,9 +50,9 @@ while ($line ne "") {
 			@array = split(/\t/, $line);
 		}
 		@uniq_caught_species = uniq(@caught_species);
-		if (@uniq_caught_species == @species && $c <= 1000) {	# All target species were caught together with less than 1000 non-target species
-			print ("Primer_pair $primerID caught all target species and less than 1000 non-target ones\n");
-			`grep -P \'^$primerID\t\' $ARGV[2] >>best_candidates.tab`;
+		if (@uniq_caught_species == @species && $c <= $ARGV[3]) {   # All target species were caught together with less than 1000 non-target species
+			print ("Primer_pair $primerID caught all target species and less than $ARGV[3] non-target sequences\n");
+			`grep -P \'^$primerID\t\' $ARGV[2] >>best_candidates.tsv`;
 			print ("--\n");
 		}
 
