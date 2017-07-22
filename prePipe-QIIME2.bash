91e44b2677e5d2c699aae47c19758bb1f7914595
Elton Vasconcelos <eltonjrv@gmail.com>
2017-07-21 17:44:33 -0700

    Update prePipe-QIIME2.bash

--- prePipe-QIIME2.bash
+++ prePipe-QIIME2.bash
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+# Programmer: Elton Vasconcelos, DVM, PhD
 ### This is a pilot pipeline using both ad-hoc PERL scripts (placed in the "bin" branch) and QIIME2 tools (https://qiime2.org).
 ### Accesories files used in some commands are placed within "accFiles" branch
 ### If you use this whole tool or part of it, please cite this github page acknowledging the author (Vasconcelos, EJ) as well as QIIME2.
